c6235fb432b9de9c9999550da4a7ab71876af170
TonyFord2002 <97993837+TonyFord2002@users.noreply.github.com>
2022-03-02 13:37:46 -0500

    Tip Code

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
-const { get } = require('http')
 const app = express()
+const port = process.env.PORT
 
 const fs = require('fs') 
+const res = require('express/lib/response')
+const req = require('express/lib/request')
 app.engine('hypatia', (filePath, options, callback) => { 
   fs.readFile(filePath, (err, content) => {
     if (err) return callback(err)
@@ -16,16 +19,24 @@ app.engine('hypatia', (filePath, options, callback) => {
     return callback(null, rendered)
   })
 })
+
 app.set('views', './views') 
-app.set('view engine', 'hypatia') 
+app.set('view engine', 'hypatia')
 
 app.get('/greeting/:name', (req, res) => {
-	res.render('template2', { title: 'Welcome', message: "Hello, stranger!", content: 'Welcome, ' + req.params.name + ' make yourself at home!'})
+	res.render('template2', { title: 'Greeting', message: "Hello, stranger!", content: 'Welcome, ' + req.params.name + ' make yourself at home!'})
   })
 
+app.get('/tip/:total/:tipPercentage' , (req, res)=>{
+    res.render('template2',{title: 'Tip', message: 'Your tip should be:', content: '$' + Math.round((req.params.total*(req.params.tipPercentage/100))*100)/100})
+})
 
+let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
+app.get('/magic/:question', (req,res)=>{
+    res.render('template2', { title: 'Magic 8 ball', message: "Your Queston: " + req.params.question, content: "Your answer is: " + answers[(Math.floor(Math.random()*19)+1)]})
+})
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log('listening');
 });
